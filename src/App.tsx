44cbaa189af126e8eb3148c9ee5c23f218d3e2a0
devat-youtuber <battuosai01@gmail.com>
2021-11-09 09:10:29 +0700

    Authentication Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,42 @@
 import Header from 'components/header'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Switch, Route, useHistory } from 'react-router-dom'
 
 import PageRender from './PageRender'
 
+import { onAuthStateChanged, sendEmailVerification, signOut } from 'firebase/auth'
+import { auth } from 'Firebase'
 
 
 const App = () => {
+
+  const history = useHistory()
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if(user){
+        if(!user.emailVerified){
+          await sendEmailVerification(user)
+          await signOut(auth)
+          return history.push("/email_verified");
+        }
+
+        console.log(user)
+      }
+    })
+
+    return unsubscribe;
+  },[])
+
   return (
-    <BrowserRouter>
+    <div>
       <Header />
       <Switch>
         <Route path="/" component={PageRender} exact />
         <Route path="/:page" component={PageRender} exact />
         <Route path="/:page/:id" component={PageRender} exact />
       </Switch>
-    </BrowserRouter>
+    </div>
   )
 }
 
